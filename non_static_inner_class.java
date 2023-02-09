6942319039f694b73143e5625e2d49db8bc9489d
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-09 09:35:51 +0530

    Create non_static_inner_class.java

---
+++ non_static_inner_class.java
@@ -0,0 +1,21 @@
+import non_static_innerclass.Biscuits;
+
+public class non_static_innerclass {
+    int x=5;
+    class Biscuits{
+        public void show_x()
+        {
+            System.out.println("this is inner class b"+x);
+        }
+    }
+}
+
+class Test
+{
+    public static void main(String args[])
+    {
+        non_static_innerclass obj=new non_static_innerclass();
+        non_static_innerclass.Biscuits obj2=obj.new Biscuits();
+        obj2.show_x();
+    }
+}
